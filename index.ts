c5bc3fa542837313e5908e3e5186dfbf06f63110
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-03-27 20:11:20 +0530

    installing eslint to do typesafety for any in req parm

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import express from 'express';
 const app = express()
 const port = 3000
 
-app.get('/ping', (req, res) => res.send('pong'))
+app.get('/ping', (_req, res) => res.send('pong1'))
 app.listen(port, () => console.log(`app listening on port ${port}!`))
